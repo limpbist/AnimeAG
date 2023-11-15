5eac1762d66a58e0b14f63c4b23e1076eb1a5ecd
Aaron <gaspar_aaron>
2023-11-15 01:10:56 -0500

    feature:BOOT-2023 first commit

---
+++ AnimeAG/AnimeAG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimeAG
+//
+//  Created by Aaron Andres Gaspar Peña on 15/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello word")
+    }
+
+
+}
+
